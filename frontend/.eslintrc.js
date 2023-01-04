c81a9554b2bd3e90ede5dff5d1155dd26f5b2003
stanislawsztrajt <stanislawsztrajt@wp.pl>
2023-01-04 13:15:40 +0100

    Changed margins in auth pages, added some texts in home page

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,8 +6,8 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier"
+        // "plugin:@typescript-eslint/recommended",
+        // "prettier"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,38 @@
 import React, { FC } from 'react';
 
 const Index: FC = () => {
-  return <h1>Home</h1>;
+  return (
+    <main className='mt-32'>
+      <section className=''>
+        <h1 className='text-6xl font-semibold'>Storing the texts in your mind</h1>
+        <p className='text-lg'>
+          write your texts and translate them with the help of one of the best translators 
+          - deepl api, texts are saved in the database and used in later functionalities 
+        </p>
+      </section>
+
+      <section className=''>
+        <h2 className='text-5xl'>Translating and saving texts</h2>
+        <p className=''>
+          Desc
+        </p>
+      </section>
+
+      <section className=''>
+        <h3 className='text-5xl'>Flash cards</h3>
+        <p className=''>
+          Desc
+        </p>
+      </section>
+
+      <section className=''>
+        <h4 className='text-5xl'>English quotes and stories</h4>
+        <p className=''>
+          Desc
+        </p>
+      </section>
+    </main>
+  );
 };
 
 export default Index;
