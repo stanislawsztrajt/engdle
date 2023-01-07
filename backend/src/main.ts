cf444d3a4586d2dbfb794cf9378488cb9ae3bdbd
stanislawsztrajt <stanislawsztrajt@wp.pl>
2023-01-07 17:24:02 +0100

    prod chacnge

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-//
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
 
-  app.enableCors({
-    origin: '*',
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-    credentials: true,
-  });
 
   await app.listen(1337);
 }
