68e20aecd4d6834834a9186e950b202269002d4a
stanislawsztrajt <stanislawsztrajt@wp.pl>
2023-01-08 18:47:00 +0100

    Prod change

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,28 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.enableCors({
-  //   origin: true,
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   credentials: true,
-  // });
-  const whitelist = ['https://engdle.vercel.app', 'http://engdle.vercel.app'];
-  app.enableCors({
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      console.log("allowed cors for:", origin)
-      callback(null, true)
-    } else {
-      console.log("blocked cors for:", origin)
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-  methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
-  credentials: true,
-  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://engdle.vercel.app', 'http://engdle.vercel.app'],
+    methods: ['POST', 'GET', 'PATCH', 'DELETE'],
+    credentials: true,
+  });
 
   await app.listen(1337);
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -10,7 +10,7 @@ import usersServices from 'utils/api/users-services';
 
 const Index: FC = () => {
   useEffect(() => {
-    console.log("test1")
+    console.log("test2")
     console.log(usersServices.getAll())
   }, [])
 
