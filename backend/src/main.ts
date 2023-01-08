bd088d128143744cbb22e7e9a245c924f47ff8b8
stanislawsztrajt <stanislawsztrajt@wp.pl>
2023-01-08 18:17:13 +0100

    Prod change

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,14 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://engdle.vercel.app', 'http://engdle.vercel.app'],
-    methods: '*',
-    allowedHeaders: '*',
-    preflightContinue: true,
-  });
 
   await app.listen(1337);
 }
