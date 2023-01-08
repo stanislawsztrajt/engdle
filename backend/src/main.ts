79d99afa8405e68959d1f02186ab34acef9272fc
stanislawsztrajt <stanislawsztrajt@wp.pl>
2023-01-08 18:23:36 +0100

    prod change

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
 
