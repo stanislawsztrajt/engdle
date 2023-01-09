e40bfe5f4a0400ee465a806173b3a545df83f9dd
stanislawsztrajt <stanislawsztrajt@wp.pl>
2023-01-09 21:52:43 +0100

    Prod change

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,16 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors(
-    {
-      preflightContinue: true,
-      origin: '*',
-      methods: '*',
-      allowedHeaders: '*'
-    }
-  );
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
 
--- frontend/src/utils/api/auth-services.ts
+++ frontend/src/utils/api/auth-services.ts
@@ -10,13 +10,7 @@ export interface IloginResponse {
 
 class AuthServices {
   async login(user: { email: string; password: string }): Promise<IloginResponse> {
-    const { data } = await axios.post(`${url}/login`, user, {
-      headers: {
-        'Access-Control-Allow-Origin': "*",
-        'Access-Control-Allow-Methods': "POST, GET, OPTIONS, DELETE, PUT",
-        'Access-Control-Allow-Headers': "append,delete,entries,foreach,get,has,keys,set,values,Authorization"
-      }
-    });
+    const { data } = await axios.post(`${url}/login`, user);
     return data;
   }
 }
